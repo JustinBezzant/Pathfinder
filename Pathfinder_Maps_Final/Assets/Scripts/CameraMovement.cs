26d6263bf17e6b9b9bd1f528f0c10d186489915e
Justin Bezzant <justinbezzant@gmail.com>
2020-08-03 10:44:51 -0600

    Initial Re-Upload

---
+++ Pathfinder_Maps_Final/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float transVelUp;
+    private float transVelDown;
+    private float transVelLeft;
+    private float transVelRight;
+    public float transAccel;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transVelUp = transVelUp + transAccel;
+            gameObject.transform.Translate(0, 0, transVelUp);
+        }
+        else
+        {
+            transVelUp = 0;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transVelDown = transVelDown - transAccel;
+            gameObject.transform.Translate(0, 0, transVelDown);
+
+        }
+        else
+        {
+            transVelDown = 0;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transVelLeft = transVelLeft - transAccel;
+            gameObject.transform.Translate(transVelLeft, 0, 0);
+
+        }
+        else
+        {
+            transVelLeft = 0;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transVelRight = transVelRight + transAccel;
+            gameObject.transform.Translate(transVelRight, 0, 0);
+
+        }
+        else
+        {
+            transVelRight = 0;
+        }
+    }
+}
---
+++ Pathfinder_Maps_Final/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float maxZoom;
+    public float maxZoomOut;
+    public float scrollSpeed;
+    private float cameraMove;
+    private Vector3 mousePos;
+
+    void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0) == true)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z - maxZoom));
+            print(mousePos);
+            print(transform.position);
+            print(Vector3.MoveTowards(transform.position, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, maxZoom)), scrollSpeed));
+
+        }
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            cameraMove = new Vector3((mousePos.x + transform.position.x) / 2, (mousePos.y + transform.position.y) / 2, (maxZoom + transform.position.z) / 2);
+            transform.Translate(cameraMove);
+        }*/
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            cameraMove = -1f * scrollSpeed;
+            if (transform.position.z > maxZoom)
+            {
+                transform.Translate(new Vector3(0, -1 * scrollSpeed, 0));
+            }
+        }
+        else if (Input.mouseScrollDelta.y < 0)
+        {
+            cameraMove = scrollSpeed;
+            if (transform.position.z < maxZoomOut)
+            {
+                transform.Translate(new Vector3(0, scrollSpeed, 0));
+            }
+        }
+    }
+}
